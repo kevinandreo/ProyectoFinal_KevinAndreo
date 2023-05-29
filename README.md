5f34f053fb4e15e841750b8dd062308157afdcc4
Kevinandreo <doradox98@gmail.com>
2023-05-29 06:35:52 -0300

    Actualizando el proyecto

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
-# ProyectoFinal_KevinAndreo
+Para correr el proyecto se necesita :
+
+1)instalar el ambiente virtual
+
+2)instalar django
+
+3)instalar pillow
+
+4)Con esto ya estaria, lo siguiente sera la prueba del video de como funciona el blog
--- blog_project/blog/tests.py
+++ blog_project/blog/tests.py
@@ -1,3 +1,22 @@
 from django.test import TestCase
+from django.contrib.auth.models import User
+from .models import Articulo
 
-# Create your tests here.
+class ArticuloTestCase(TestCase):
+    def setUp(self):
+        self.user = User.objects.create_user(username='testuser', password='testpassword')
+        self.articulo = Articulo.objects.create(
+            titulo='Título de prueba',
+            subtitulo='Subtítulo de prueba',
+            cuerpo='Cuerpo de prueba',
+            autor=self.user
+        )
+
+    def test_articulo_str(self):
+        self.assertEqual(str(self.articulo), 'Título de prueba')
+
+    def test_articulo_autor(self):
+        self.assertEqual(self.articulo.autor, self.user)
+
+
+
